4c2e8ff33bede0a32ef99725aea6070c89c5e984
AmauryEsparza <amauryesparza@gmail.com>
2015-07-02 15:31:01 -0500

    set ArrayAdapter and Item click listener

--- app/src/main/java/com/example/amauryesparza/serviceinfoviewer/MainActivity.java
+++ app/src/main/java/com/example/amauryesparza/serviceinfoviewer/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.amauryesparza.serviceinfoviewer;
 
 import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,6 +12,11 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(savedInstanceState == null){
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new MainActivityFragment())
+                    .commit();
+        }
     }
 
 
--- app/src/main/java/com/example/amauryesparza/serviceinfoviewer/MainActivityFragment.java
+++ app/src/main/java/com/example/amauryesparza/serviceinfoviewer/MainActivityFragment.java
@@ -1,16 +1,23 @@
 package com.example.amauryesparza.serviceinfoviewer;
 
+import android.app.ActivityManager;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment {
+public class MainActivityFragment extends Fragment implements AdapterView.OnItemClickListener {
+
+    private ArrayAdapter<RunningServiceWrapper> mServices;
+    private ListView mServiceList;
 
     public MainActivityFragment() {
     }
@@ -18,6 +25,33 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        mServices = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        mServiceList = (ListView) rootView.findViewById(R.id.service_list);
+        mServiceList.setAdapter(mServices);
+        mServiceList.setOnItemClickListener(this);
+        return rootView;
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        //  TODO: Return here and hook this up
+    }
+
+    private class RunningServiceWrapper{
+
+        private ActivityManager.RunningServiceInfo mInfo;
+
+        public RunningServiceWrapper(ActivityManager.RunningServiceInfo info){
+            this.mInfo = info;
+        }
+
+        public ActivityManager.RunningServiceInfo getInfo(){
+            return mInfo;
+        }
+
+        public String toString(){
+            return mInfo.service.flattenToShortString();
+        }
     }
 }
